04976dd592fb329e499aec0ee9d1b3d67a3da033
realkosmoss <113364969+realkosmoss@users.noreply.github.com>
2022-09-14 20:18:21 +0200

    Update scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -1,2 +1,5 @@
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/realkosmoss/websot/main/retard/islands/cletus.lua'),true))() 
 -- cletus fucker
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/realkosmoss/websot/main/retard/islands/cf.lua'),true))()
+-- cf only keybinds
