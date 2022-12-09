25e21eec878c9b58bff3b43c5c9fc9217dae2771
realkosmoss <113364969+realkosmoss@users.noreply.github.com>
2022-12-09 16:12:27 +0100

    Create AutoLauncher.lua

---
+++ AutoLauncher.lua
@@ -0,0 +1,6 @@
+if game.PlaceId == 10914683361 then -- busy business
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/realkosmoss/scripts/main/scripts/BusyBusinessNotDone')))()
+end
+if game.PlaceId == 10042772221 then -- lumber unsecure hub
+    loadstring(game:HttpGet(('https://kosmos-scripts.000webhostapp.com/pub/lomberticoon.lua')))()
+end
