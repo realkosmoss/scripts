78542c06235e3131d374c1ee44433b3a6784e0ae
realkosmoss <113364969+realkosmoss@users.noreply.github.com>
2022-12-18 00:59:12 +0100

    Update AutoLauncher.lua

--- AutoLauncher.lua
+++ AutoLauncher.lua
@@ -4,3 +4,6 @@ end
 if game.PlaceId == 10042772221 then -- lumber unsecure hub
     loadstring(game:HttpGet(('https://kosmos-scripts.000webhostapp.com/pub/lomberticoon.lua')))()
 end
+if game.PlaceId == 5902977746 then -- Ultimate Tower Defense
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/realkosmoss/scripts/main/scripts/UltimateTowerDefense.lua')))()
+end
