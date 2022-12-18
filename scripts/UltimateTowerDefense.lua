a2a419427cb87302a9d380a4a67cfb8e0b22e6a0
realkosmoss <113364969+realkosmoss@users.noreply.github.com>
2022-12-18 01:19:58 +0100

    Update UltimateTowerDefense.lua

--- scripts/UltimateTowerDefense.lua
+++ scripts/UltimateTowerDefense.lua
@@ -2,6 +2,9 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHept
 local Window = Library.CreateLib("Ultimate Tower Defense", "DarkTheme")
 local Tab = Window:NewTab("Farms")
 local Section = Tab:NewSection("Farms")
+Section:NewButton("Remove Catched Fish GUI", "yes", function()
+    game:GetService("Players").LocalPlayer.PlayerGui.MainGui.MainFrames.CaughtFish:Destroy()
+end)
 Section:NewToggle("Fish Farm", "lolol", function(state)
     if state then
     _G.loop = true
@@ -17,5 +20,30 @@ end
 end
     end
 end)
--- really ugly code LMFAO pls no het ;() 
--- i will add more features later if someone even sees this LMFAO
+Section:NewButton("Delete Crate GUI", "yes", function()
+    game:GetService("Players").LocalPlayer.PlayerGui.MainGui.MainFrames.CrateReward:Destroy()
+    game:GetService("Players").LocalPlayer.PlayerGui.MessagesGui:Destroy()
+end)
+Section:NewToggle("Auto Crates", "lolol", function(state)
+    if state then
+    _G.loop = true
+    while _G.loop do
+        local args = {
+            [1] = 1
+        }
+        game:GetService("ReplicatedStorage").Modules.GlobalInit.RemoteEvents.PlayerBuyTower:FireServer(unpack(args))
+   wait(0.5)
+end
+    else
+        _G.loop = false
+    while _G.loop do
+        local args = {
+            [1] = 1
+        }
+        game:GetService("ReplicatedStorage").Modules.GlobalInit.RemoteEvents.PlayerBuyTower:FireServer(unpack(args))
+   wait(0.5)
+end
+    end
+end)
+
+-- really ugly code LMFAO pls no het ;()
