eeb5471f6e95e6207ba1367e0b842890284d4c68
realkosmoss <113364969+realkosmoss@users.noreply.github.com>
2023-04-09 01:54:44 +0200

    Update AutoLauncher.lua

--- AutoLauncher.lua
+++ AutoLauncher.lua
@@ -1,23 +1,4 @@
-if game.PlaceId == 10914683361 then -- busy business
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/realkosmoss/scripts/main/scripts/BusyBusinessNotDone')))()
-end
-if game.PlaceId == 10042772221 then -- lumber unsecure hub
-    loadstring(game:HttpGet(('https://kosmos-scripts.000webhostapp.com/pub/lomberticoon.lua')))()
-end
-if game.PlaceId == 5902977746 then -- Ultimate Tower Defense
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/realkosmoss/scripts/main/scripts/UltimateTowerDefense.lua')))()
-end
-if game.PlaceId == 6279039046 then -- Sled Simulator
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/realkosmoss/scripts/main/scripts/SledSimulatorAutoFarm.lua')))()
-end
-if game.PlaceId == 11108419601 then -- slide into rdite lol
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(5041.19482421875, -14121.47265625, -3000.751220703125)
-wait(1)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(5020.57421875, -14121.8662109375, -3012.02197265625)
-end
-if game.PlaceId == 7952502098 then -- impossible glass shit https://www.roblox.com/games/7952502098/Impossible-Glass-Bridge-Obby
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/realkosmoss/scripts/main/scripts/ImpossibleGlassBridgeObby.lua')))()
-end
-if game.PlaceId == 1962086868 then -- tower off hell
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/realkosmoss/scripts/main/scripts/TowerOfHell.lua')))()
-end
+local CustomLoader = loadstring(game:HttpGet("https://raw.githubusercontent.com/realkosmoss/scripts/main/loader.lua"))()
+local SET_UP = CustomLoader("Kosmos Loader", "json",{
+   URL = "https://raw.githubusercontent.com/realkosmoss/scripts/main/games.json";
+})
